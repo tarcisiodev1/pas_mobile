e4f91ecaaead0823bbc20138ea281f1f65ae107e
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2024-05-05 16:32:11 -0300

    app1

--- App.js
+++ App.js
@@ -1,11 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { openDatabase } from "expo-sqlite";
+
+const db = openDatabase("database.db");
 
 export default function App() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [email, setEmail] = useState("");
+  const [idade, setIdade] = useState("");
+  const [endereco, setEndereco] = useState("");
+
+  const handleSubmit = async () => {
+    console.log("Dados dos pais:", { nome, sobrenome, email, idade, endereco });
+    // const response = await fetch("https://meu-servidor.com/api/pais", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     nome,
+    //     sobrenome,
+    //     email,
+    //     idade,
+    //     endereco,
+    //   }),
+    // });
+
+    //limpar campos
+    setNome("");
+    setSobrenome("");
+    setEmail("");
+    setIdade("");
+    setEndereco("");
+
+    // if (response.ok) {
+    //   console.log("Dados enviados com sucesso!");
+
+    //   setNome("");
+    //   setSobrenome("");
+    //   setEmail("");
+    //   setIdade("");
+    //   setEndereco("");
+    // } else {
+    //   console.error("Erro ao enviar os dados.");
+    // }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.navbar}>
+        <Text style={styles.navbarTitle}>Sagah</Text>
+      </View>
+      <View style={styles.container1}>
+        <Text style={styles.title}>Na prática</Text>
+        <Text style={styles.label}>Nome:</Text>
+        <TextInput style={styles.input} value={nome} onChangeText={setNome} />
+        <Text style={styles.label}>Sobrenome:</Text>
+        <TextInput
+          style={styles.input}
+          value={sobrenome}
+          onChangeText={setSobrenome}
+        />
+        <Text style={styles.label}>E-mail:</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        <Text style={styles.label}>Idade:</Text>
+        <TextInput
+          style={styles.input}
+          value={idade}
+          onChangeText={setIdade}
+          keyboardType="numeric"
+        />
+        <Text style={styles.label}>Endereço:</Text>
+        <TextInput
+          style={styles.input}
+          value={endereco}
+          onChangeText={setEndereco}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Enviar</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +101,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // padding: 20,
+  },
+  container1: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  navbar: {
+    backgroundColor: "purple",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  navbarTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: "100%",
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
   },
 });
